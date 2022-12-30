0fb9c5b976eb03a0c5e0c3e8f7013c26aac667cb
chaehun <chaehun@uniess.co.kr>
2022-12-30 10:09:25 +0900

    feat : @ComponentScan scope package 설정

--- src/main/kotlin/kr/co/houmuch/batch/HoumuchBatchApplication.kt
+++ src/main/kotlin/kr/co/houmuch/batch/HoumuchBatchApplication.kt
@@ -1,16 +1,25 @@
 package kr.co.houmuch.batch
 
 import kr.co.houmuch.batch.config.properties.ContractOpenApiProperties
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+inline fun <reified T> logger(): Logger = LoggerFactory.getLogger(T::class.java)
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = [BASE_PACKAGE])
+@EntityScan(basePackages = [BASE_PACKAGE])
+@EnableJpaRepositories(basePackages = [BASE_PACKAGE])
 @EnableConfigurationProperties(ContractOpenApiProperties::class)
 @EnableBatchProcessing
 class HoumuchBatchApplication
+
+const val BASE_PACKAGE = "kr.co.houmuch"
 
 fun main(args: Array<String>) {
     runApplication<HoumuchBatchApplication>(*args)
